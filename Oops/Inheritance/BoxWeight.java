b25c18a7953dd45b6b57ef66fc139de38c078af5
Mukul Pretham <mukulpretham@gmail.com>
2024-09-09 19:11:06 +0530

    inheritance update

--- Oops/Inheritance/BoxWeight.java
+++ Oops/Inheritance/BoxWeight.java
@@ -2,22 +2,17 @@ package Oops.Inheritance;
 
 public class BoxWeight extends Box {
     double weight;
-    double price;
+
     BoxWeight(){
         this.weight= 2;
     }
+
     BoxWeight(double length , double height , double width, double weight){
         super(length,height,width);
         //super() is used to call the super class or parent constructor, if it's not mentioned
         //the default Box() constructor is called.
         //This is one use of super.
         this.weight= weight;
-    }
-
-    //Another example in which the super() calls Box(parent)  constructor with single argument.
-    BoxWeight(int side, int price ){
-        super(side);
-        this.price= price;
     }
 
     //another use of super is to refer the variable of super class
