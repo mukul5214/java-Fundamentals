b661277f587450807f93675a3183d16a848e7e42
mukul <mukulpretham@gmail.com>
2024-10-22 18:22:59 +0530

    multithreading updated

--- Oops/Multithreading/Main.java
+++ Oops/Multithreading/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Multi1 t1 = new Multi1();
         Multi2 t2 = new Multi2();
-        t1.start();
         t2.start();
+        t1.start();//These 2 threads are not sheduled so the thread which executes faster will
+                   // get executed first
     }
 }
