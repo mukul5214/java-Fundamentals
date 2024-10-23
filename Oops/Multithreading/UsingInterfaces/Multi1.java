9b60d45fca74bf14ad8f8a8a712462dddf17ce2e
mukul <mukulpretham@gmail.com>
2024-10-23 09:49:58 +0530

    Multithreading by implementing Runnable interface

---
+++ Oops/Multithreading/UsingInterfaces/Multi1.java
@@ -0,0 +1,14 @@
+package Oops.Multithreading.UsingInterfaces;
+
+public class Multi1 implements Runnable {
+    public void run(){
+        for (int i = 0 ; i  < 10 ; i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ Oops/Multithreading/UsingInterfaces/Multi2.java
@@ -0,0 +1,14 @@
+package Oops.Multithreading.UsingInterfaces;
+
+public class Multi2 implements Runnable{
+    public void run(){
+        for (int i = 0 ; i < 10 ;i++){
+            System.out.println("Mukul");
+            try {
+                Thread.sleep(500);//sleep is a static method so we access it through class itself
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
