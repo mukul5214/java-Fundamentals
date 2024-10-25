62fd3c783156592236b938481ee171a2c07e38b5
mukul <mukulpretham@gmail.com>
2024-10-25 20:52:42 +0530

    MultiThreading - points added in Priority package

--- Oops/Multithreading/Priority/Main.java
+++ Oops/Multithreading/Priority/Main.java
@@ -10,16 +10,27 @@ public class Main {
         System.out.println("Default Priority of t0: " + t0.getPriority());
 
         // Set the main thread priority (though this may not affect the other threads)
-        Thread.currentThread().setPriority(5); // Changing the priority of the main thread to 5
+        Thread.currentThread().setPriority(3); // Changing the priority of the main thread to 5
         System.out.println("Main thread priority: " + Thread.currentThread().getPriority());
+
+        System.out.println(t0.getPriority());
 
         // Set thread priorities
         t0.setPriority(2);  // Lower priority (less likely to get CPU time)
         t1.setPriority(10); // Higher priority (more likely to get CPU time)
+
 
         // Start the threads
         t0.start();
         t1.start();
     }
 }
+
+/*By default every thread priority is 5 (priority ranges from 1 to 10*/
+
+/* all the thread made in the main thread are its child threads
+* the priority of a thread is the priority of the main thread when it creates the child thread
+* */
+
+/* If 2 threads have same priority then its up to java scheduler in JVM*/
 
