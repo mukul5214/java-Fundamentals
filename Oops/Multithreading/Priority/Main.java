6159208a636a6c03e71f0f933abbc687cd74cc41
mukul <mukulpretham@gmail.com>
2024-10-25 21:21:50 +0530

    MT update

--- Oops/Multithreading/Priority/Main.java
+++ Oops/Multithreading/Priority/Main.java
@@ -10,15 +10,19 @@ public class Main {
         System.out.println("Default Priority of t0: " + t0.getPriority());
 
         // Set the main thread priority (though this may not affect the other threads)
-        Thread.currentThread().setPriority(3); // Changing the priority of the main thread to 5
+        Thread.currentThread().setPriority(3); // Changing the priority of the main thread to 3
         System.out.println("Main thread priority: " + Thread.currentThread().getPriority());
 
-        System.out.println(t0.getPriority());
+        System.out.println(t0.getPriority());//Returns int(Priority)
 
-        // Set thread priorities
-        t0.setPriority(2);  // Lower priority (less likely to get CPU time)
-        t1.setPriority(10); // Higher priority (more likely to get CPU time)
-
+        // Set thread priorities (Between 1 and 10)
+        try{
+            t0.setPriority(2);  // Lower priority (less likely to get CPU time)
+            t1.setPriority(10);  // Higher priority (more likely to get CPU time)
+        }catch (IllegalArgumentException e ){
+            System.out.println("the arguments should be between one and ten");
+            //IllegalArgumentException is thrown if the arguments are not in between 1-10.
+        }
 
         // Start the threads
         t0.start();
@@ -28,9 +32,10 @@ public class Main {
 
 /*By default every thread priority is 5 (priority ranges from 1 to 10*/
 
+/* If 2 threads have same priority then its up to java scheduler in JVM*/
+
 /* all the thread made in the main thread are its child threads
 * the priority of a thread is the priority of the main thread when it creates the child thread
 * */
 
-/* If 2 threads have same priority then its up to java scheduler in JVM*/
 
