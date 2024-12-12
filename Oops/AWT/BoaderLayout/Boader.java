73c28bc0c0fb5f96bc16558c47883e68e93864b5
Mukul Pretham <mukulpretham@gmail.com>
2024-12-12 20:13:41 +0530

    layout

---
+++ Oops/AWT/BoaderLayout/Boader.java
@@ -0,0 +1,32 @@
+package Oops.AWT.BoaderLayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Boader {
+    JFrame f;
+    Boader(){
+        f = new JFrame();
+        JButton b1 = new JButton("North");
+        JButton b2 = new JButton("south");
+        JButton b3 = new JButton("east");
+        JButton b4 = new JButton("west");
+        JButton b5 = new JButton("Center");
+
+        f.setLayout(new BorderLayout(15,20));
+        f.add(b1, BorderLayout.NORTH);
+        f.add(b2, BorderLayout.SOUTH);
+        f.add(b3, BorderLayout.EAST);
+        f.add(b4, BorderLayout.WEST);
+        f.add(b5, BorderLayout.CENTER);
+
+        f.setSize(300,300);
+        f.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Boader b = new Boader();
+    }
+
+}
